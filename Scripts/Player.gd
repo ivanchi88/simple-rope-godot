78dc1d508a80f7be8b1a7a22f348f10b97eb6529
ivan <ivanmotos_8@hotmail.es>
2021-04-12 21:29:09 +0200

    Added a moving character that throws a rope

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,41 +1,59 @@
 extends KinematicBody2D
 
-var speed = Vector2(0, 0) 
+export (int) var speed = 800
+export (int) var jump_speed = -900
+export (int) var gravity = 4000 
+var velocity = Vector2.ZERO
+
 var rope = preload("res://Scenes/Rope.tscn") 
 var hasRope = false 
 var ropee = null
+
+var cooldown = 10
+var passed = cooldown
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
-	speed = move_and_slide(speed * 1000)
-	speed = Vector2.ZERO
+func _physics_process(delta):
+	
+	
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
 
+	passed += delta
+
+	if(ropee != null):
+		ropee.firstPosition = position
 
 
 func _input(event):
+	velocity.x = 0
 	if(Input.is_key_pressed(KEY_A)):
-		speed.x = -1
-	if(Input.is_key_pressed(KEY_D)):
-		speed.x = 1
+		velocity.x -= speed
 		$sprite.flip_h = true
-	if(Input.is_key_pressed(KEY_W)):
-		speed.y = -1
-	if (Input.is_key_pressed(KEY_S)):
-		speed.y = 1
+	if(Input.is_key_pressed(KEY_D)):
+		velocity.x += speed
+		$sprite.flip_h = false
+	if(Input.is_key_pressed(KEY_W)): 
+		if is_on_floor():
+			velocity.y = jump_speed
 	
-	speed = speed.normalized() 
+	
+	speed = speed 
 
-	if (Input.is_key_pressed(KEY_SPACE)):
+	if (cooldown < passed && Input.is_mouse_button_pressed(BUTTON_LEFT)):
 		var mouse = get_viewport().get_mouse_position()
 		var ropeDir = (mouse - global_position).normalized()
-		var ropeInstance = ropee
+		var ropeInstance = ropee 
 		if(ropeInstance == null):
 			ropeInstance = rope.instance()
 			ropee = ropeInstance
 
-		ropeInstance.speed = ropeDir.normalized() * 10
-		ropeInstance.position = global_position
+		ropeInstance.accel = ropeDir.normalized() * 4000
+		ropee.firstPosition = position
+
 		get_parent().add_child(ropeInstance)
+
+		passed = 0
--- Scripts/Rope.gd
+++ Scripts/Rope.gd
@@ -2,64 +2,54 @@ extends Node2D
 
 var hook = preload("res://Scenes/Hook.tscn") 
 
-var ropeLength = 100
+var ropeLength = 50
+const numOfIterations = 8
+const maxDistanceBetweenPoints = 5
+
 var  particlesPosition:  PoolVector2Array = PoolVector2Array()
 var  oldParticlesPosition: PoolVector2Array = PoolVector2Array()
 var	 accumulatedForces : PoolVector2Array = PoolVector2Array()
 
-var  segmentShapes = []
-
 var gravity = Vector2(0, 98)
 
-const numOfIterations = 8
-const maxDistanceBetweenPoints = 1
-
-const initialPosition = Vector2(0, 0)  
-
+const initialPosition = Vector2(0, 0)   
 var firstPosition = initialPosition 
 
-var colitions : StaticBody2D = null
+var accel: Vector2 = Vector2.ZERO
+var drag: float = 0.9
 
-var speed: Vector2 = Vector2.ZERO
-var nextExpectedParabolicPosition = Vector2.ZERO
-var time = 0
+var hookable_layer = 16
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	colitions = StaticBody2D.new() 
-	self.add_child(colitions)
+
 	particlesPosition.resize(ropeLength)
 	oldParticlesPosition.resize(ropeLength)
 	accumulatedForces.resize(ropeLength) 
 
 	for i in range(ropeLength):
-		particlesPosition[i] = Vector2(initialPosition)
-		oldParticlesPosition[i] = Vector2(initialPosition)
-		accumulatedForces[i] = gravity
-		if(i < ropeLength - 1):
-			var particleShape = SegmentShape2D.new()
-			particleShape.a = particlesPosition[i]
-			particleShape.b = particlesPosition[i + 1]
-			var shape = CollisionShape2D.new()
-			shape.set_shape(particleShape)
-			segmentShapes.append(particleShape)
-			colitions.add_child(shape)
+		particlesPosition[i] = Vector2(firstPosition)
+		oldParticlesPosition[i] = Vector2(firstPosition)
+		accumulatedForces[i] = gravity + accel
+
 func _physics_process(delta): 
-	calculateParabolicMovement(delta)
+
+	calculateAcceleration(delta)
+
 	accumulateForces()
 	verlet(delta)
 	satisfyConstraints()
 
 func accumulateForces():
 	for i in range(ropeLength):
-		accumulatedForces[i] = gravity
+		accumulatedForces[i] = gravity + accel
 
 func verlet(delta) : 
 	for i in range(ropeLength) :
 		var position = particlesPosition[i]
 		var oldPosition = oldParticlesPosition[i]
 		
-		var acceleration = Vector2.ZERO
+		var acceleration = accumulatedForces[i]
 		var newPosition = position + (position - oldPosition + acceleration * delta * delta)
 
 		particlesPosition[i] = newPosition
@@ -85,25 +75,16 @@ func satisfyConstraints():
 			var newX1 = x1 + (distance * 0.5 * diff)
 			var newX2 =  x2 - (distance * 0.5 * diff) 
 
-			#check rope colisions
-			var collisions = space.intersect_point(newX1)
+			#check rope colisions with global colision
+			var collisions = space.intersect_point(global_position + newX1, 32, [], hookable_layer)
 			if (collisions.size() > 0):
 				var correction = newX1 - oldParticlesPosition[i]
 				newX1 = x1 - correction * 2 #BOUNCE! 
 
 			particlesPosition[i] = newX1
-			particlesPosition[i + 1] = newX2
+			particlesPosition[i + 1] = newX2 
 
-			segmentShapes[i].a = newX1
-			segmentShapes[i].b = newX2
-
-		particlesPosition[0] = firstPosition
-		segmentShapes[0].a = firstPosition
-	
-		if (abs(particlesPosition[-1].distance_to(particlesPosition[-2])) < maxDistanceBetweenPoints):
-			particlesPosition[-1] = nextExpectedParabolicPosition 
-			segmentShapes[-2].b = nextExpectedParabolicPosition
-		pass
+		particlesPosition[0] = firstPosition 
 
 
 func _draw():
@@ -120,17 +101,10 @@ func _process(_delta):
 
 func _input(event):
 	# Mouse in viewport coordinates.
-	if event is InputEventMouseButton && event.is_pressed():  
-			firstPosition = event.position 
+	pass
 
-func calculateParabolicMovement (delta):
-	time += delta
-	var x = speed.x * time 
-	var y = (speed.y * time) + (gravity.y * time * time)/2
-
-	speed.y = gravity.y * time
-
-	nextExpectedParabolicPosition = Vector2(x, y) 
+func calculateAcceleration(delta): 
+	accel = accel * (drag - delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
